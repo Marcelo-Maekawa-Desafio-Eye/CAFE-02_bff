fdd41d44137aaddc43f95717a37675652890ac27
mrMaekawa <mrmaekawa@gmail.com>
2025-05-15 00:32:33 -0300

    chore(nest.js): initial setup for BFF project with CI/CD pipeline

---
+++ src/config/configuration.ts
@@ -0,0 +1,6 @@
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 3000,
+    jwtSecret: process.env.JWT_SECRET || 'defaultSecret',
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN || '3600s',
+  });
+  
